b71abc4f2d333b8c043a83c604fa9a7c97a279c1
Iwan H Prasetyo <55027286+IwanHPrasetyo@users.noreply.github.com>
2020-02-15 11:36:45 +0700

    fix Realtime Data Update

--- server/server.js
+++ server/server.js
@@ -9,19 +9,24 @@ const port = process.env.PORT || 8888;
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
+
 var jumlahKonekServer = 0;
+var userOnline = "User Online";
+
 app.use(express.static(publicPath));
 
 io.on("connection", socket => {
   jumlahKonekServer++;
   io.sockets.emit("jumlahKonekServer", {
-    jumlahKonekServer: jumlahKonekServer
+    jumlahKonekServer: jumlahKonekServer,
+    userOnline: userOnline
   });
   console.log("koneksi berjalan");
   socket.on("disconnect", () => {
     jumlahKonekServer--;
     io.sockets.emit("jumlahKonekServer", {
-      jumlahKonekServer: jumlahKonekServer
+      jumlahKonekServer: jumlahKonekServer,
+      userOnline: userOnline
     });
     console.log("koneksi berhenti");
   });
